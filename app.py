0956ba16f26b8598512d5c36ac94de85764dd769
TirthManavadariya <tirthpatelp1@gmail.com>
2026-09-15 01:20:15 +0530

    chore: format app.py

--- app.py
+++ app.py
@@ -14,3 +14,4 @@ from backend.app import app
 if __name__ == '__main__':
     print("Starting Roomee Discovery Engine from root entrypoint on http://localhost:5000 ...")
     app.run(host='0.0.0.0', port=5000, debug=False)
+
